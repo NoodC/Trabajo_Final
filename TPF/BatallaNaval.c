6e459a6a0885e3e6005d40be8982ecd01b3664f6
Facu <131072626+NoodC@users.noreply.github.com>
2024-04-18 06:26:44 -0400

    Reemplazado el codigo, expandida la funcion de colocar barco, todo documentado, se llega

--- TPF/BatallaNaval.c
+++ TPF/BatallaNaval.c
@@ -1,203 +1,258 @@
-// Primera Entrega Trabajo Practico Final - Lucas Jara y Guillermo Laguardia
+//Trabajo Practico Final - Lucas Jara y Guillermo Laguardia
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAM_TABLERO 21
-/*
-Cada casilla es una estructura que almacena dos valores enteros para cada casilla: la existencia o ausencia de un barco y si fue atacada o no. 
-*/
-struct casilla {
-	int existe_barco;
-	int fue_atacada;
+#define STARTING_GOLD 115
+#define STARTING_AMMO 100
+
+/*Cada casilla  es un stuct que contiene un valor que indica si hay un barco o no y su tipo, y un valor que indica si la casilla ha sido bombardeada o no*/
+
+struct square{
+	int is_ship;
+	int bombed;
 };
 
-/* 
-La funcion para colocar barcos por ahora recibe como parametro un tablero, en el cual el jugador selecciona el tipo de barco, 
-su orientacion y ubicacion , verificando que no exceda los limites del tablero. Queda pendiente la implementacion del sistema de oro 
-y una verificacion exhaustiva de cada casilla, posiblemente mediante otra funcion externa
+/*
+Para referencia: (valor asignado;tipo;area)(la mina a efectos practicos es un barco mas)
+1;Portaaviones;2x7
+2;Buque;1x7
+3;Submarino;1x5
+4;Crucero;1x5
+5;Lancha;1x2
+6;Mina;1x1
 */
 
-int colocar(struct casilla tablero[TAM_TABLERO][TAM_TABLERO]){
-	printf("Elija el barco a colocar \n 1 para Portaaviones (2x7) \n 2 para Buque (1x7) \n 3 para Submarino (1x5) \n 4 para Crucero (1x5) \n 5 para Lancha (1x2): ");
-    int seleccion, orientacion;
-    scanf("%d", seleccion);
-    printf("Elija la orientacion: 1 para horizontal, 2 para vertical:\n");
-    scanf("%d", orientacion);
-    printf("Ingrese las coordenadas (X,Y).\n El tablero es de 21x21.\n La casilla corresponde a la esquina superior izquierda del barco.");
-    switch (seleccion){
+/*Funcion que coloca un barco, recibe como parametros el tablero, el tipo de barco, la orientacion y las coordenadas (x,y)
+Devuelve un valor que indica la validez de la colocacion
+Notar que el barco siempre sera colocado a la derecha o abajo de la coordenada (x,y)*/
+//***Esta funcion puede ir a un archivo de cabecera en el futuro***
+//***PENDIENTE: Expandir los errores, colocar mensajes en la terminal***
+int placement(struct square board[21][21], int type, int rotation, int x, int y){
+	int i, j, length, width;
+    switch(type){ // -1 si no se puede colocar, -2 si excede el tablero, -3 si ya hay un barco en esa casilla, -4 si el tipo de barco no existe, 0 si se coloca correctamente
         case 1:
-            int fila, columna;
-            scanf("Fila: %d", fila);
-            fila -= 1;
-            scanf("Columna: %d", columna);
-            columna -= 1;
-            if ((orientacion == 1)&&((columna > 14)||(fila < 19))){
-                printf("El barco no puede ser colocado en estas coordenadas");
-                return(-1);
-            }else if((orientacion == 2)&&((columna > 19)||(fila > 14))){
-                printf("El barco no puede ser colocado en estas coordenadas");
-                return(-1);
-            }
-            if (orientacion == 1){
-                for (int i = fila; i < fila + 2; i++){
-                    for (int j = columna; j < columna + 7; j++){
-                        tablero[i][j].existe_barco = 1;
-                    };
-                };
-                return(0);
-            }else if(orientacion == 2){
-                for (int i = fila; i < fila + 7; i++){
-                    for (int j = columna; j < columna + 2; j++){
-                        tablero[i][j].existe_barco = 1;
+            length = 7;
+            width = 2;
+            if ((rotation != 1) && (rotation != 2)) return -1;
+            if (rotation == 1){
+                if (((x+length) >= 21) || ((y+width) >= 21)) return -2;
+                for (i = x; i < x+length; i++){
+                    for (j = y; j < y+width; j++){
+                        if (board[i][j].is_ship != 0) return -3;
                     }
                 }
-                return(0);
-            }else{
-                printf("Error, revisar las entradas");
-                return(-1);
-                };
+                for (i = x; i < x+length; i++){
+                    for (j = y; j < y+width; j++){
+                        board[i][j].is_ship = 1;
+                    }
+                }
+            } else {
+                if (((x+width) >= 21) || ((y+length) >= 21)) return -2;
+                for (i = x; i < x+width; i++){
+                    for (j = y; j < y+length; j++){
+                        if (board[i][j].is_ship != 0) return -3;
+                    }
+                }
+                for (i = x; i < x+width; i++){
+                    for (j = y; j < y+length; j++){
+                        board[i][j].is_ship = 1;
+                    }
+                }
+            }
+            return 0;
             break;
         case 2:
-            int fila, columna;
-            scanf("Fila: %d", fila);
-            fila -= 1;
-            scanf("Columna: %d", columna);
-            columna -= 1;
-            if ((orientacion == 1)&&((columna > 14)||(fila < 20))){
-                printf("El barco no puede ser colocado en estas coordenadas");
-                return(-1);
-            }else if((orientacion == 2)&&((columna > 20)||(fila > 14))){
-                printf("El barco no puede ser colocado en estas coordenadas");
-                return(-1);
-            }
-            if (orientacion == 1){
-                for (int i = fila; i < fila + 1; i++){
-                    for (int j = columna; j < columna + 7; j++){
-                        tablero[i][j].existe_barco = 1;
-                    };
-                };
-                return(0);
-            }else if(orientacion == 2){
-                for (int i = fila; i < fila + 1; i++){
-                    for (int j = columna; j < columna + 2; j++){
-                        tablero[i][j].existe_barco = 1;
+            length = 7;
+            width = 1;
+            if ((rotation != 1) && (rotation != 2)) return -1;
+            if (rotation == 1){
+                if (((x+length) >= 21) || ((y+width) >= 21)) return -2;
+                for (i = x; i < x+length; i++){
+                    for (j = y; j < y+width; j++){
+                        if (board[i][j].is_ship != 0) return -3;
                     }
                 }
-                return(0);
-            }else{
-                printf("Error, revisar las entradas");
-                return(-1);
-                };
+                for (i = x; i < x+length; i++){
+                    for (j = y; j < y+width; j++){
+                        board[i][j].is_ship = 1;
+                    }
+                }
+            } else {
+                if (((x+width) >= 21) || ((y+length) >= 21)) return -2;
+                for (i = x; i < x+width; i++){
+                    for (j = y; j < y+length; j++){
+                        if (board[i][j].is_ship != 0) return -3;
+                    }
+                }
+                for (i = x; i < x+width; i++){
+                    for (j = y; j < y+length; j++){
+                        board[i][j].is_ship = 1;
+                    }
+                }
+            }
+            return 0;
             break;
         case 3:
-            int fila, columna;
-            scanf("Fila: %d", fila);
-            fila -= 1;
-            scanf("Columna: %d", columna);
-            columna -= 1;
-            if ((orientacion == 1)&&((columna > 16)||(fila < 20))){
-                printf("El barco no puede ser colocado en estas coordenadas");
-                return(-1);
-            }else if((orientacion == 2)&&((columna > 20)||(fila > 16))){
-                printf("El barco no puede ser colocado en estas coordenadas");
-                return(-1);
-            }
-            if (orientacion == 1){
-                for (int i = fila; i < fila + 1; i++){
-                    for (int j = columna; j < columna + 5; j++){
-                        tablero[i][j].existe_barco = 1;
-                    };
-                };
-                return(0);
-            }else if(orientacion == 2){
-                for (int i = fila; i < fila + 1; i++){
-                    for (int j = columna; j < columna + 5; j++){
-                        tablero[i][j].existe_barco = 1;
+            length = 5;
+            width = 1;
+            if ((rotation != 1) && (rotation != 2)) return -1;
+            if (rotation == 1){
+                if (((x+length) >= 21) || ((y+width) >= 21)) return -2;
+                for (i = x; i < x+length; i++){
+                    for (j = y; j < y+width; j++){
+                        if (board[i][j].is_ship != 0) return -3;
                     }
                 }
-                return(0);
-            }else{
-                printf("Error, revisar las entradas");
-                return(-1);
-                };
+                for (i = x; i < x+length; i++){
+                    for (j = y; j < y+width; j++){
+                        board[i][j].is_ship = 1;
+                    }
+                }
+            } else {
+                if (((x+width) >= 21) || ((y+length) >= 21)) return -2;
+                for (i = x; i < x+width; i++){
+                    for (j = y; j < y+length; j++){
+                        if (board[i][j].is_ship != 0) return -3;
+                    }
+                }
+                for (i = x; i < x+width; i++){
+                    for (j = y; j < y+length; j++){
+                        board[i][j].is_ship = 1;
+                    }
+                }
+            }
+            return 0;
             break;
         case 4:
-            int fila, columna;
-            scanf("Fila: %d", fila);
-            fila -= 1;
-            scanf("Columna: %d", columna);
-            columna -= 1;
-            if ((orientacion == 1)&&((columna > 16)||(fila < 20))){
-                printf("El barco no puede ser colocado en estas coordenadas");
-                return(-1);
-            }else if((orientacion == 2)&&((columna > 20)||(fila > 16))){
-                printf("El barco no puede ser colocado en estas coordenadas");
-                return(-1);
-            }
-            if (orientacion == 1){
-                for (int i = fila; i < fila + 1; i++){
-                    for (int j = columna; j < columna + 5; j++){
-                        tablero[i][j].existe_barco = 1;
-                    };
-                };
-                return(0);
-            }else if(orientacion == 2){
-                for (int i = fila; i < fila + 1; i++){
-                    for (int j = columna; j < columna + 5; j++){
-                        tablero[i][j].existe_barco = 1;
+            length = 5;
+            width = 1;
+            if ((rotation != 1) && (rotation != 2)) return -1;
+            if (rotation == 1){
+                if (((x+length) >= 21) || ((y+width) >= 21)) return -2;
+                for (i = x; i < x+length; i++){
+                    for (j = y; j < y+width; j++){
+                        if (board[i][j].is_ship != 0) return -3;
                     }
                 }
-                return(0);
-            }else{
-                printf("Error, revisar las entradas");
-                return(-1);
-                };
+                for (i = x; i < x+length; i++){
+                    for (j = y; j < y+width; j++){
+                        board[i][j].is_ship = 1;
+                    }
+                }
+            } else {
+                if (((x+width) >= 21) || ((y+length) >= 21)) return -2;
+                for (i = x; i < x+width; i++){
+                    for (j = y; j < y+length; j++){
+                        if (board[i][j].is_ship != 0) return -3;
+                    }
+                }
+                for (i = x; i < x+width; i++){
+                    for (j = y; j < y+length; j++){
+                        board[i][j].is_ship = 1;
+                    }
+                }
+            }
+            return 0;
             break;
         case 5:
-            int fila, columna;
-            scanf("Fila: %d", fila);
-            fila -= 1;
-            scanf("Columna: %d", columna);
-            columna -= 1;
-            if ((orientacion == 1)&&((columna > 19)||(fila < 20))){
-                printf("El barco no puede ser colocado en estas coordenadas");
-                return(-1);
-            }else if((orientacion == 2)&&((columna > 20)||(fila > 19))){
-                printf("El barco no puede ser colocado en estas coordenadas");
-                return(-1);
-            }
-            if (orientacion == 1){
-                for (int i = fila; i < fila + 1; i++){
-                    for (int j = columna; j < columna + 2; j++){
-                        tablero[i][j].existe_barco = 1;
-                    };
-                };
-                return(0);
-            }else if(orientacion == 2){
-                for (int i = fila; i < fila + 1; i++){
-                    for (int j = columna; j < columna + 2; j++){
-                        tablero[i][j].existe_barco = 1;
+            length = 2;
+            width = 1;
+            if ((rotation != 1) && (rotation != 2)) return -1;
+            if (rotation == 1){
+                if (((x+length) >= 21) || ((y+width) >= 21)) return -2;
+                for (i = x; i < x+length; i++){
+                    for (j = y; j < y+width; j++){
+                        if (board[i][j].is_ship != 0) return -3;
                     }
                 }
-                return(0);
-            }else{
-                printf("Error, revisar las entradas");
-                return(-1);
-                };
+                for (i = x; i < x+length; i++){
+                    for (j = y; j < y+width; j++){
+                        board[i][j].is_ship = 1;
+                    }
+                }
+            } else {
+                if (((x+width) >= 21) || ((y+length) >= 21)) return -2;
+                for (i = x; i < x+width; i++){
+                    for (j = y; j < y+length; j++){
+                        if (board[i][j].is_ship != 0) return -3;
+                    }
+                }
+                for (i = x; i < x+width; i++){
+                    for (j = y; j < y+length; j++){
+                        board[i][j].is_ship = 1;
+                    }
+                }
+            }
+            return 0;
+            break;
+        default:
+            return -4;
             break;
         }
-    };
+}
 
 
+int main()
+{
+    struct square myboard[21][21]; //Inicializacion del tablero del usuario
+    int i, j;
+    for (i = 0; i < 21; i++){
+        for (j = 0; j < 21; j++){
+            myboard[i][j].is_ship = 0;
+            myboard[i][j].bombed = 0;
+        }
+    }
+    int player1gold = STARTING_GOLD;
+    int player1ammo = STARTING_AMMO;
+    int finishedplacing = 0;
+    int finishedloop = 0;
+    while(((player1gold > 5) || (finishedplacing == 0))&&(finishedloop == 2)){ //El loop se completa por lo menos dos veces, debido a la bandera finishedloop
+        int type, x, y, rotation;
+        printf("Ingrese el tipo de barco (1-5), 0 para terminar, si ya ha colocado un barco: ");
+        scanf("%d", &type);
+        switch(type){
+            case 0:
+                finishedplacing = 0;
+                break;
+            case 1:
+                if(player1gold < 30) printf("Oro insuficiente\n");
+                break;
+            case 2:
+                if(player1gold < 20) printf("Oro insuficiente\n");
+                break;
+            case 3:
+                if(player1gold < 50) printf("Oro insuficiente\n");
+                break;
+            case 4:
+                if(player1gold < 10) printf("Oro insuficiente\n");
+                break;
+            case 5:
+                if(player1gold < 5) printf("Oro insuficiente\n");
+                break;
+            default:
+                printf("Tipo de barco no valido\n");
+                break;
+        }
+        printf("Inserte la coordenada x donde desea colocar el barco: \n");
+        scanf("%d", &x);
+        printf("Inserte la coordenada y donde desea colocar el barco: \n");
+        scanf("%d", &y);
+        printf("Inserte la orientacion del barco (1=Horizontal, 2=Vertical): \n");
+        scanf("%d", &rotation);
+        finishedplacing = placement(myboard, type, x, y, rotation); //No es necesario verificar aqui los valores de entrada, ya que placement devuelve valores de error en esos casos
+        finishedloop++;
+    }
+    
+    struct square theirboard[21][21]; //Inicializacion del tablero del rival
+    for (i = 0; i < 21; i++){
+        for (j = 0; j < 21; j++){
+            theirboard[i][j].is_ship = 0;
+            theirboard[i][j].bombed = 0;
+        }
+    }
 
-int main(void) {
-	struct casilla tablero[TAM_TABLERO][TAM_TABLERO]; // Este bloque de codigo genera un tablero vacio
-	for (int i = 0; i < TAM_TABLERO; i++){
-		for (int j = 0; i < TAM_TABLERO; j++){
-			tablero[i][j].existe_barco = 0;
-			tablero[i][j].fue_atacada = 0;
-		}
-	}
-	return EXIT_SUCCESS;
+    
+
+    return 0;
 }
